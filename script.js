6261ba41b315c46df1087f1523ed9a77ebaf82f1
Alexandre Garcia <134382188+atxmagnus@users.noreply.github.com>
2023-08-17 06:13:14 -0300

    add index.html write basic html structure to index.html add script.js write pseudocode to script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+//create a function to randomly generate either 'Rock', 'Paper' or 'Scissors'
+
+
+
+//declare a function to to compare the user input versus the randomly generated and return the winner
+// get the user input with prompt
+// the user input must be case-insensitive
+// call the function to generate randomly
+// compare who winned
+//return the winner of the round
+
+//make a loop to call the functions 5 times
+// keep scores and report who won the most
