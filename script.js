013465a94a3126a42aeb3f76ae96a4dfc49cf289
Alexandre <134382188+atxmagnus@users.noreply.github.com>
2023-08-21 06:47:30 -0300

    Fix the switch statement in the rng function Write a function to compare results Write a function to get the user input Treat the user input to be case-insensitive Make a loop to invoke the functions 5 times Keep scores while in the loop Display who won the most and the results

--- script.js
+++ script.js
@@ -1,28 +1,99 @@
+let userChoice;
+let computerChoice;
+let player = computer = tie = 0;
+
 //create a function to randomly generate either 'Rock', 'Paper' or 'Scissors'
 function generateComputerChoice() {
 
-    let randomGenerated = Math.floor(Math.random() * 3);
+    let rng = Math.floor(Math.random() * 3);
 
-    switch (randomGenerated) {
-        case 1:
-            randomGenerated = "Rock";
+    switch (rng) {
+        case 0:
+            computerChoice = "Rock";
             break;
-        case 2:
-            randomGenerated = "Paper";
+        case 1:
+            computerChoice = "Paper";
             break;
         default:
-            randomGenerated = "Scissor";
+            computerChoice = "Scissors";
             break;
     }
-    return randomGenerated;
 }
 
 //declare a function to to compare the user input versus the randomly generated and return the winner
-// get the user input with prompt
-// the user input must be case-insensitive
-// call the function to generate randomly
-// compare who winned
-//return the winner of the round
+function compareUserInput(choice) {
+
+    // call the function to generate randomly
+    generateComputerChoice();
+
+    // get the user input with prompt
+    getUserInput();
+    if (userChoice == "Rock" && computerChoice == "Rock") {
+        return "tie";
+    }
+    else if (userChoice == "Rock" && computerChoice == "Scissors") {
+        return "player";
+    }
+    else if (userChoice == "Rock" && computerChoice == "Paper") {
+        return "computer";
+    }
+    else if (userChoice == "Paper" && computerChoice == "Paper") {
+        return "tie";
+    }
+    else if (userChoice == "Paper" && computerChoice == "Rock") {
+        return "player";
+    }
+    else if (userChoice == "Paper" && computerChoice == "Scissors") {
+        return "computer";
+    }
+    else if (userChoice == "Scissors" && computerChoice == "Scissors") {
+        return "tie";
+    }
+    else if (userChoice == "Scissors" && computerChoice == "Paper") {
+        return "player";
+    }
+    else if (userChoice == "Scissors" && computerChoice == "Rock") {
+        return "computer";
+    }
+    else {
+        return "invalid";
+    }
+}
+
+function getUserInput() {
+    userChoice = prompt("Rock, Paper or Scissors?");
+    if (userChoice != null) {
+        //The user input must be case-insensitive, makes the first letter upper case and the rest lower case for further comparison
+        userChoice = userChoice.charAt(0).toUpperCase() + userChoice.slice(1).toLowerCase();
+    }
+    else
+        alert("You cancelled!");
+}
 
 //make a loop to call the functions 5 times
+for (let x = 0; x < 5; x++) {
+    let winner = compareUserInput();
+    if (winner == "player") {
+        alert("The player winned!");
+        player++;
+    }
+    else if (winner == "computer") {
+        alert("The computer winned!");
+        computer++;
+    }
+    else if (winner == "tie") {
+        alert("Tie!");
+        tie++;
+    }
+}
+
 // keep scores and report who won the most
+if (player > computer) {
+    alert("The player has won the most!\nPlayer: " + player + "\nComputer: " + computer + "\nTie: " + tie);
+}
+else if (computer > player) {
+    alert("The computer has won the most!\nComputer: " + computer + "\nPlayer: " + player + "\nTie: " + tie);
+}
+else {
+    alert("Tie! No one has won!\nTie: " + tie + "\nPlayer: " + player + "\nComputer: " + computer);
+}
