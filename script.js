d4798fba95958bb73bcbce01784521c4ccf562f8
Alexandre Garcia <134382188+atxmagnus@users.noreply.github.com>
2024-01-02 07:28:19 -0300

    Put script to load after the body to fix the DOM  manipulation Create the buttons and put event listeners to call the functions Count the number of rounds played Create a function to display results (still not using it)

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 let userChoice;
 let computerChoice;
-let player = computer = tie = 0;
+let player = computer = tie = rounds = 0;
 //make a loop to call the functions 5 times
 // CREATE BUTTON Rock
 const rock = document.createElement('button');
+rock.innerHTML = "Rock";
 document.body.appendChild(rock);
-// CREATE BUTTON PAPER
-// CREATE BUTTON SCISSORS
-// ADD EVENT LISTENER TO BUTTON ROCK THAT CALLS THE FUNCTION AS ROCK AS THE PARAMETER
-// ADD EVENT LISTENER TO BUTTON PAPER THAT CALLS THE FUNCTION AS PAPER AS THE PARAMETER
-// ADD EVENT LISTENER TO BUTTON SCISSORS THAT CALLS THE FUNCTION AS SCISSORS AS THE PARAMETER
+rock.addEventListener("click", function () { compareUserInput("Rock") });
+const paper = document.createElement('button');
+paper.innerHTML = "Paper";
+document.body.appendChild(paper);
+paper.addEventListener("click", function () { compareUserInput("Paper") });
+const scissor = document.createElement('button');
+scissor.innerHTML = "Scissor";
+document.body.appendChild(scissor);
+scissor.addEventListener("click", function () { compareUserInput("Scissor") });
 // CREATE A DIV TO DISPLAY RESULTS
 // ONCE 5 ROUNDS WERE PLAYED ANNOUNCE A WINNER
 
@@ -32,54 +37,55 @@ function generateComputerChoice() {
 }
 
 //declare a function to to compare the user input versus the randomly generated and return the winner
-function compareUserInput(choice) {
-
+function compareUserInput(userChoice) {
+    rounds++;
     // call the function to generate randomly
     generateComputerChoice();
 
     // get the user input with prompt
-    getUserInput();
     if (userChoice == "Rock" && computerChoice == "Rock") {
-        return "tie";
+        tie++;
     }
     else if (userChoice == "Rock" && computerChoice == "Scissors") {
-        return "player";
+        player++;
     }
     else if (userChoice == "Rock" && computerChoice == "Paper") {
-        return "computer";
+        computer++;
     }
     else if (userChoice == "Paper" && computerChoice == "Paper") {
-        return "tie";
+        tie++;
     }
     else if (userChoice == "Paper" && computerChoice == "Rock") {
-        return "player";
+        player++
     }
     else if (userChoice == "Paper" && computerChoice == "Scissors") {
-        return "computer";
+        computer++;
     }
     else if (userChoice == "Scissors" && computerChoice == "Scissors") {
-        return "tie";
+        tie++;
     }
     else if (userChoice == "Scissors" && computerChoice == "Paper") {
-        return "player";
+        player++;
     }
     else if (userChoice == "Scissors" && computerChoice == "Rock") {
-        return "computer";
-    }
-    else {
-        return "invalid";
+        computer++;
     }
 }
 
 
 
 // keep scores and report who won the most
-if (player > computer) {
-    alert("The player has won the most!\nPlayer: " + player + "\nComputer: " + computer + "\nTie: " + tie);
-}
-else if (computer > player) {
-    alert("The computer has won the most!\nComputer: " + computer + "\nPlayer: " + player + "\nTie: " + tie);
-}
-else {
-    alert("Tie! No one has won!\nTie: " + tie + "\nPlayer: " + player + "\nComputer: " + computer);
+function displayResults() {
+    const results = document.createElement('div');
+
+    if (player > computer) {
+        results.innerHTML = "The player has won the most!\nPlayer: " + player + "\nComputer: " + computer + "\nTie: " + tie;
+    }
+    else if (computer > player) {
+        results.innerHTML = "The computer has won the most!\nComputer: " + computer + "\nPlayer: " + player + "\nTie: " + tie;
+    }
+    else {
+        results.innerHTML = "Tie! No one has won!\nTie: " + tie + "\nPlayer: " + player + "\nComputer: " + computer;
+    }
+    document.body.appendChild(results);
 }
